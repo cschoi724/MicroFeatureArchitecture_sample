4f312733d6a33f46696d4ecf2306d90342798ac5
cschoi724 <cschoi724@gamil.com>
2023-12-13 12:00:30 +0900

    first commit

---
+++ Targets/MicroFeachureArchSampleKit/Tests/MicroFeachureArchSampleKitTests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class MicroFeachureArchSampleKitTests: XCTestCase {
+    func test_example() {
+        XCTAssertEqual("MicroFeachureArchSampleKit", "MicroFeachureArchSampleKit")
+    }
+}
---
+++ Targets/MicroFeachureArchSampleUI/Tests/MicroFeachureArchSampleUITests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class MicroFeachureArchSampleUITests: XCTestCase {
+    func test_example() {
+        XCTAssertEqual("MicroFeachureArchSampleUI", "MicroFeachureArchSampleUI")
+    }
+}
